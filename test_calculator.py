1978424919b639dd4b3c725ee0ad35ee5f628a8d
ziccotine <23018379@myrp.edu.sg>
2024-05-24 14:34:27 +0800

    Add files via upload

---
+++ test_calculator.py
@@ -0,0 +1,54 @@
+from calculator.calculator import Calculator
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+
+    def test_subtract(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.subtract(a, b)
+
+        # assert
+        expected = 3087
+        assert result == expected
+
+    def test_multiply(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.multiply(a, b)
+
+        # assert
+        expected = 5332114
+        assert result == expected
+
+    def test_divide(self):
+        # arrange
+        a = 500
+        b = 5
+        cal = Calculator()
+
+        # act
+        result = cal.divide(a, b)
+
+        # assert
+        expected = 100
+        assert result == expected
